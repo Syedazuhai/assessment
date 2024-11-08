227baa60976e4ca40c34aff189b690961f0fdea0
Syedazuhai <aaroobahnoor@gmail.com>
2024-11-08 13:41:29 +0000

    Add files via upload

---
+++ calc.py..py
@@ -0,0 +1,12 @@
+num_1 = float(input("Enter the first number:"))
+num_2 = float(input("Enter the second number:"))
+
+sum_up = num_1 + num_2
+difference = num_1 - num_2
+product = num_1 * num_2
+quotient = num_1 / num_2
+print("sum:" , sum_up)
+print("Difference:" + str(difference))
+print("product:" + str(product))
+print("Quotient:" + str(quotient))
+    
